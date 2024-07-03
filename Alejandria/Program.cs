6b9697c16d489a51d437c9df3b73178b9e89d873
Arialdodev <arialdodiaz8@gmail.com>
2024-07-03 08:54:00 -0500

    chore: :lipstick: layout file configuration

--- Alejandria/Program.cs
+++ Alejandria/Program.cs
@@ -40,6 +40,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Autor}/{action=Listar}/{id?}");
 
 app.Run();
